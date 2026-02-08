8e858362f478e768ea08918c3ab344270a4bdcee
Luhan Casa <luhanmarques50@gmail.com>
2026-02-08 19:34:18 -0300

    fix3

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ocorrencia
 
-# Register your models here.
+@admin.register(Ocorrencia)
+class OcorrenciaAdmin(admin.ModelAdmin):
+    # Isso faz aparecer as colunas bonitinhas no admin
+    list_display = ('descricao', 'local', 'status')
+    list_filter = ('status', 'local')
+    search_fields = ('descricao', 'local')
